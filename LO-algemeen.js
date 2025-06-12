f367e700875b8d0282c77b8dc9c7f81c50e82342
Muurthe <mirtheverdurmen@hotmail.com>
2025-06-12 16:43:19 +0200

    select van bewijs gekleurd

--- LO-algemeen.js
+++ LO-algemeen.js
@@ -1,5 +1,18 @@
 function showContent(id) {
-    const contents = document.querySelectorAll('.content > div');
-    contents.forEach(div => div.classList.add('hidden'));
-    document.getElementById(id).classList.remove('hidden');
+  // Verberg alle content-divs
+  const contents = document.querySelectorAll('.content > div');
+  contents.forEach(div => div.classList.add('hidden'));
+
+  // Toon de geselecteerde
+  document.getElementById(id).classList.remove('hidden');
+
+  // Verwijder 'selected' van alle navigatielinks
+  const links = document.querySelectorAll('.Leernav a');
+  links.forEach(link => link.classList.remove('selected'));
+
+  // Voeg 'selected' toe aan de geklikte link
+  const clickedLink = Array.from(links).find(link => link.getAttribute('onclick')?.includes(id));
+  if (clickedLink) {
+    clickedLink.classList.add('selected');
   }
+}
